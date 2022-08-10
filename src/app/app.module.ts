1ba474ddcf47ebab92338b4459801ca139ea4b37
Aleyna Celik <clkkaley@gmail.com>
2022-08-10 19:04:39 +0300

    how create component todo component (html,css,ts)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToDoComponent } from './todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToDoComponent,
+    
   ],
   imports: [
     BrowserModule
