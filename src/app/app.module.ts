ca8c190399d0ec5882f96a3fe4a58200d762b3ad
Aleyna Celik <clkkaley@gmail.com>
2022-08-10 19:09:11 +0300

    how created component in terminal (ng g c todo)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ToDoComponent } from './todo.component';
+import { TodoComponent } from './todo/todo.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToDoComponent,
+    TodoComponent,
+
     
   ],
   imports: [
